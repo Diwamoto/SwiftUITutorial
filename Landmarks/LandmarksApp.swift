0f54b1de0ddd960edc46839f3189cc996737bbb3
Diwamoto <daiki.work.0118@gmail.com>
2022-03-21 02:28:09 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by DaikiIwamoto on 2022/03/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
