0666e1fd7107fc638e9bb8b0f41d747519b2b037
Diwamoto <daiki.work.0118@gmail.com>
2022-03-21 16:15:54 +0900

    [add] Swift UI tutorial 3 Handling User Input

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by DaikiIwamoto on 2022/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
